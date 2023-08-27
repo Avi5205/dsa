231e90cbc5d2967708244aec8c49ede3bd41071d
Avinash Kumar <akgpcamp@gmail.com>
2023-08-27 19:12:22 +0530

    doubly linkedlist

---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,49 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        tail = tail.prev;
+        tail.next = null;
+        temp.prev = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+}
---
+++ src/DoublyLinkedList/Main.java
@@ -0,0 +1,9 @@
+package DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList(7);
+        dll.append(2);
+        dll.printList();
+    }
+}
