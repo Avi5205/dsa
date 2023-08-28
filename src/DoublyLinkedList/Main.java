8f1eff9074b8ee83599559fc269195474c7ec92e
Avinash Kumar <akgpcamp@gmail.com>
2023-08-28 11:33:49 +0530

    doubly linkedlist

--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList(7);
         dll.append(2);
+
+        dll.prepend(1);
+
         dll.printList();
     }
 }
